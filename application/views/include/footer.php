efbf298a0ab3b33027ba806661744ba1be13ac75
Yohanes Boyoh <boyohh@hotmail.com>
2016-10-11 15:04:26 +0700

    update footer

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -3,5 +3,5 @@
 
 
 <div id = "footer-copyright"  class = "col-lg-6 col-lg-offset-3 text-center">
-	<span>Copyright &copy; 2016 Vista Handal Sejahtera.</span>
+	<span>Copyright &copy; 2016 Simpatindo Multi Media.</span>
 </div>
